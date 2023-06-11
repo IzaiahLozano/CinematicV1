d0e63573f626015ce532296f021adf2ab754a032
Sammy SlugAustin <achen168@ucsc.edu>
2023-06-11 00:08:05 -0700

    adding cinematic test 1

---
+++ Cinematic.js
@@ -0,0 +1,71 @@
+class Prod extends Phaser.Scene {
+    constructor() {
+        super('Production');
+    }
+
+
+
+    create() {
+
+        this.cameras.main.setBackgroundColor('#fffff1');
+
+        let mesg = this.add.text(
+            800, 
+            540,
+            "Under Construction \nThis will be our production logo and will be animated our name will be FLUX \nClick to Advance",
+            {
+               font: "45px Garamond",
+               color: "#000000",
+               wordWrap: { width: 1000 }
+            }
+         );
+
+
+         this.input.on('pointerup', () => {
+            // Advance to the next scene
+            this.scene.start('menu');
+        });
+
+    }
+
+}
+
+
+
+
+class Menu extends Phaser.Scene {
+    constructor() {
+        super('menu');
+    }
+
+
+
+    create() {
+
+        this.cameras.main.setBackgroundColor('#fffff1');
+
+        let mesg = this.add.text(
+            800, 
+            540,
+            "Under Construction \nThe menu will be next, there will be a button for the game, settings, and credits \nClick to Advance",
+            {
+               font: "45px Garamond",
+               color: "#000000",
+               wordWrap: { width: 1000 }
+            }
+         );
+
+
+         this.input.on('pointerup', () => {
+            // Advance to the next scene
+            this.scene.start('lv1');
+        });
+
+    }
+
+
+}
+
+
+
+
